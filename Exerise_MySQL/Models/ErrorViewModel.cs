d679d781d1b3babe91be80d11887d5a1bccb2674
yan <zhangyan232425@hotmail.com>
2017-12-24 12:16:11 -0500

    Initial new ASP.NET Core Web App(MVC) to practice connect  MySQL database

---
+++ Exerise_MySQL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exerise_MySQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
