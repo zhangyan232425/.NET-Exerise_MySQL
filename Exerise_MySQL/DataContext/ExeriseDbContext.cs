3b5ea213873f70cac5def03848670e6104892a28
yan <zhangyan232425@hotmail.com>
2017-12-24 15:57:33 -0500

    add DataContext

---
+++ Exerise_MySQL/DataContext/ExeriseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exerise_MySQL.DataContext
+{
+    public class ExeriseDbContext:DbContext
+    {
+        public ExeriseDbContext(DbContextOptions<ExeriseDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
